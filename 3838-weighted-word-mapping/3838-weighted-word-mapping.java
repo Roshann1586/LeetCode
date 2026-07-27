4fece75218abe81bd2307da13b3993308e77828e
Roshann <roshan.s2025@vitstudent.ac.in>
2026-07-27 13:41:40 +0530

    Time: 6 ms (16.32%), Space: 47 MB (6.81%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        String ans="";
+        for(String x:words){
+            int weight=0;
+            for(char c:x.toCharArray()){
+                weight+=(weights[c-'a']);
+            }
+            weight%=26;
+            ans+=(char)(97+26-weight-1);
+        }
+        return ans;
+    }
+}
