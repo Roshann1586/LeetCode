3628b650aaa4841286ea8305c44115297dbfed96
Roshann <roshan.s2025@vitstudent.ac.in>
2026-09-14 23:15:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (17.64%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -12,18 +12,6 @@ class Solution {
         if(rec1[1]>=rec2[3]){
             return false;
         }
-        if(rec2[2]<=rec1[0]){
-            return false;
-        }
-        if(rec2[3]<=rec1[1]){
-            return false;
-        }
-        if(rec2[0]>=rec1[2]){
-            return false;
-        }
-        if(rec2[1]>=rec1[3]){
-            return false;
-        }
         return true;
     }
 }
